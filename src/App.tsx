d5bec87e0410b5ffd079e318b4a85c909dec17ce
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2023-03-25 23:42:41 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import HelloWorld from "./Elements/HelloWorld"
-import Test from "./Elements/Test/Test"
 
 
 function App() {
   return (
     <div>
       <HelloWorld></HelloWorld>
-      <Test>
-        <div>aaa
-          <span>
-            ssss
-          </span>
-        </div>
-      </Test>
     </div>
   )
 }
